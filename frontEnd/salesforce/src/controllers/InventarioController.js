c53b74e65aa97a18b790d55632668a28d655d8fb
ramirexx <ramirolozacmj@gmail.com>
2014-09-24 00:24:09 -0400

    adicion de modales de loading y confirmacion

--- frontEnd/salesforce/src/controllers/InventarioController.js
+++ frontEnd/salesforce/src/controllers/InventarioController.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-function InventarioController($scope, $http, $cookies, $routeParams, serviceShare, paramService) {
+function InventarioController($scope, $http, $cookies, $routeParams, serviceShare, paramService, inventarioService) {
     console.log('--->admin');
     var id = $routeParams.id;
 
@@ -59,15 +59,17 @@ function InventarioController($scope, $http, $cookies, $routeParams, serviceShar
                     data: JSON.stringify($scope.formData)
                 }).success(function (response) {
                         result = response;
-                        alert(result.message)
                         $scope.formData = null
-                        $("#ModalArticulo").modal('hide')
+                    $("#ModalArticulo").modal('hide');
+                    $('#alertSucces').modal('show');
+                    $("#mensajeAlertSucces").text('Registro Guardado');
                 }). error(function (response) {   //
-                        alert("ERROR! intente mas tarde")
+                    $('#alertError').modal('show');
+                    $("#mensajeAlertError").text(data.result + 'Error! intente nuevamente')
 
                     });
             }
-           //inventarioService.saveArticulo($scope.formData);
+
 
     } else {
         //get de form by Id    //
@@ -84,72 +86,9 @@ function InventarioController($scope, $http, $cookies, $routeParams, serviceShar
             });
     }
 //Lista proveedores
-    var tableProveedores = new Array();
-    var oTable = $('#dataTableProdcutos');
-    $http.get(service+'/articulosHabilitados').success(
-        function (data, status, headers, config) {
-            $scope.tableProveedores = data.result;
-            oTable = $('#dataTableProdcutos').dataTable(
-                {
-                    "bJQueryUI":true,
-                    "bAutoWidth":true,
-                    "bProcessing":true,
-                    "oLanguage":{
-                        "sUrl":"src/js/i18n/dataTable_es.txt"
-                    },
-                    "aaData":$scope.tableProveedores,
-                    "aoColumns":[
-//                        {
-//                            "mData": "id",
-//                            "bSearchable": false,
-//                            "bVisible": false
-//                        },
-                        {
-                            "mData":null,
-                            "bSortable":  false
-                        },
-                        {
-                            "mData": "id"
-                        },
-                        {
-                            "mData":"descripcionArticulo"
-                        },
-                        {
-                            "mData":"cantidadReorden"
-                        },
-                        {
-                            "mData":"precioCosto"
-                        },
-                        {
-                            "bSortable": false,
-                            "mData":function (oObj) {
-                                var a = " <a href='#/clientes/"+oObj.id+"' class='btn btn-primary'><i class='fa fa-edit'></i></a>";
-                                return a;
-
-                            }
-
-                        },
-                        {
-                            "mData":function (oObj) {
-                                var b = " <a href='#/cliente-info/"+oObj.id+"' class='btn btn-danger'><i class='fa fa-trash-o'></i></a>";
-                                return b;
-                            }
-                        }
+    inventarioService.articuloList($scope);
 
 
-                    ],
-                    "fnRowCallback":function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-                        $('td:eq(0)', nRow).html(iDisplayIndexFull + 1);
-                        return nRow;
-                    }
-                });
-
-        }).
-        error(function (data, status, headers, config) {
-            alert(data.result);
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
 // ends proveedores
 
 //categorias begin
@@ -296,6 +235,13 @@ function InventarioController($scope, $http, $cookies, $routeParams, serviceShar
             // or server returns response with an error status.
         });
 // ends clses
+
+
+
+
+
+
+
 }
 
 
--- frontEnd/salesforce/src/services/articuloService.js
+++ frontEnd/salesforce/src/services/articuloService.js
@@ -9,6 +9,76 @@
 app.service("inventarioService", function ($http,$cookies) {
 
     return {
+        articuloList: function ($scope) {
+            var tableProveedores = new Array();
+            var oTable = $('#dataTableProdcutos');
+            $http.get(service+'/articulosHabilitados').success(
+                function (data, status, headers, config) {
+                    $scope.tableProveedores = data.result;
+                    oTable = $('#dataTableProdcutos').dataTable(
+                        {
+                            "bJQueryUI":true,
+                            "bAutoWidth":true,
+                            "bProcessing":true,
+                            "oLanguage":{
+                                "sUrl":"src/js/i18n/dataTable_es.txt"
+                            },
+                            "aaData":$scope.tableProveedores,
+                            "aoColumns":[
+//                        {
+//                            "mData": "id",
+//                            "bSearchable": false,
+//                            "bVisible": false
+//                        },
+                                {
+                                    "mData":null,
+                                    "bSortable":  false
+                                },
+                                {
+                                    "mData": "id"
+                                },
+                                {
+                                    "mData":"descripcionArticulo"
+                                },
+                                {
+                                    "mData":"cantidadReorden"
+                                },
+                                {
+                                    "mData":"precioCosto"
+                                },
+                                {
+                                    "bSortable": false,
+                                    "mData":function (oObj) {
+                                        var a = " <a href='#/clientes/"+oObj.id+"' class='btn btn-primary'><i class='fa fa-edit'></i></a>";
+                                        return a;
+
+                                    }
+
+                                },
+                                {
+                                    "mData":function (oObj) {
+                                        var b = " <a href='#/cliente-info/"+oObj.id+"' class='btn btn-danger'><i class='fa fa-trash-o'></i></a>";
+                                        return b;
+                                    }
+                                }
+
+
+                            ],
+                            "fnRowCallback":function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                                $('td:eq(0)', nRow).html(iDisplayIndexFull + 1);
+                                return nRow;
+                            }
+                        });
+
+                }).
+                error(function (data, status, headers, config) {
+                    alert(data.result);
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+//
+        },
+
         saveArticulo: function (data) {
             var result;  //
             $http({
