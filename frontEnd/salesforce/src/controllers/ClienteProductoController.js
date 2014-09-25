5c4ed8846fde566891c96fed7d06a8e28a6dcbb2
json666 <jheysonsanchez@gmail.com>
2014-09-25 21:14:00 +0200

    Cambios adicion Bodega

---
+++ frontEnd/salesforce/src/controllers/ClienteProductoController.js
@@ -0,0 +1,23 @@
+/**
+* HomeController
+*/
+function ClienteProductoController($scope, $http, $cookies, serviceShare) {
+
+    $scope.a = "INFO"
+
+    $scope.productoList = {
+        items: [
+            {
+                "clave": 1,
+                "producto": "Hydrogen",
+                "cantidad": 2,
+                "precio": 10,
+                "importe": 20
+
+            }
+
+
+
+]
+    }
+}
